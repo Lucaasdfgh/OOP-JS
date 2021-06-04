a72d2c0be7fb13d8cfc71296ff91f28a524db317
Lucaasdfgh <lucaascervantes9@gmail.com>
2021-06-04 16:28:05 -0500

    factory functions

---
+++ index.js
@@ -0,0 +1,62 @@
+//procedural programming
+const salary = 30
+const overtime = 40
+const rate = 20
+
+function getWage (salary, overtime, rate){
+    return salary * (overtime*rate)
+}
+//console.log(getWage)
+//console.log("info")
+//OOP programming 
+//encapsulation
+let employee = {
+    salary: 30,
+    overtime: 40,
+    rate: 20,
+    getWage: function(){
+        const value = this.salary * (this.overtime * this.rate)
+        console.log(value)
+        return value
+    }
+};
+employee.getWage()
+//console.log(employee.getWage())
+//Abstraction
+//inheritance
+//polymorphism
+
+//OOP
+//creating objects
+let circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y:1
+    },
+    draw: function(){
+        console.log("draw")
+    }
+}
+circle.draw()
+//factories and constructors
+//factory function
+function createSquare(radius){
+    return {
+        radius,
+        draw: function (){
+            console.log("square")
+        }
+    }
+}
+const square = createSquare(1)
+console.log(square.draw)
+//construction function
+function Triangle(radius){
+    console.log("this", this)
+    this.radius = radius
+    this.draw = function(){
+        console.log("draw")
+    }
+}
+const another = new Triangle
